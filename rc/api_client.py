f68acbcc8f45070362b22c86da598b6c24be66eb
Brent Strysko <brentstrysko@brentstrysko.com>
2014-10-23 22:14:00 -0400

    Functionality of rc-api established.  APIClient now covers nearly all the API.

--- rc/api_client.py
+++ rc/api_client.py
@@ -1,18 +1,140 @@
+import logging
 import requests
+import json
+import os
 
 class APIClient(object):
-  
-  def __init__(self, public_key, private_key, base_url="http://roboticsclub.org/api"):
+
+  def __init__(self, public_key=None, private_key=None, base_url=None):
+    """
+    Creates a API session with the provided public and private keys.
+    The URL that is used to connect to the API can be overwritten by setting
+    base_url.
+    """
+
+    self._logger = logging.getLogger(__name__)
+    self._public_key = public_key
+
+    if not public_key:
+      public_key = os.environ.get('RC_PUBLIC_KEY')
+
+    if not private_key:
+      private_key = os.environ.get('RC_PRIVATE_KEY')
+
+    if not base_url:
+      base_url = "http://roboticsclub.org/crm"
+
     self._session = requests.Session()
     self._session.headers.update({
-      "X_PROJECT_ID": public_key,
-      "X_PROJECT_NAME": private_key,
+      "PUBLIC_KEY": public_key,
+      "PRIVATE_KEY": private_key,
     })
+
     self._base_url = base_url
 
-  @property
-  def users(self):
-    return self._get_request("users")
+  def users(self, **kwargs):
+    return self._api_query_get_request("users", **kwargs)
 
-  def _get_request(self, url):
-    return self._session.get("{}/{}".format(self._base_url, url)).json()
+  def projects(self, **kwargs):
+    return self._api_query_get_request("projects", **kwargs)
+
+  def officers(self, **kwargs):
+    return self._api_query_get_request("officers", **kwargs)
+
+  def webcams(self, **kwargs):
+    return self._api_query_get_request("webcams", **kwargs)
+
+  def social_medias(self, **kwargs):
+    return self._api_query_get_request("social_medias", **kwargs)
+
+  def channels(self, **kwargs):
+    return self._api_query_get_request("channels", **kwargs)
+
+  def calendar(self):
+    return self._api_get_request("calendar")
+
+  def login(self, username, password):
+    return self._post_request("login", {
+      'username': username,
+      'password': password
+    })
+
+  def datetime(self):
+    # TODO: convert to datetime
+    return self._api_get_request("datetime")
+
+  def magnetic(self, _id):
+    """
+    Returns the ID of the user with the specified Magnetic ID.
+    """
+
+    return self._post_request("magnetic", _id)
+
+  def rfid(self, _id):
+    """
+    Returns the ID of the user with the specified RFID.
+    """
+
+    return self._post_request("rfid", _id)
+
+  # TODO: ability to create and write to channels
+
+  def get(self, url):
+    """
+    Request a resource using GET with authentication keys.
+    """
+
+    return self._get(url)
+
+
+  def set_user_rfid(self, user_id, rfid):
+    """
+    Set the RFID for the specified user.
+    This is a priveleged option and you likely
+    do not have permission.
+    """
+
+    return self._post_request("users/{}/rfid".format(user_id), rfid)
+
+
+  # Private Methods
+
+  def _get(self, url):
+    response = self._session.get(url)
+    
+    if response.status_code != requests.codes.ok:
+      raise requests.exceptions.HTTPError(response.text)
+
+    return response
+
+  def _get_request(self, path):
+    return self._get("{}/{}".format(self._base_url, path))
+
+  def _api_get_request(self, path):
+    return self._get_request("api/{}".format(path)).json()
+
+  def _api_query_get_request(self, path, **kwargs):
+    url = path
+
+    for i,(key, value) in enumerate(kwargs.items()):
+      token = '?' if i == 0 else '&'
+      url += "{}{}={}".format(token, key, value)
+
+    return self._api_get_request(url)
+
+  def _post_request(self, path, message):
+    url = "{}/api/{}/".format(self._base_url, path)
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+    try:
+      data = json.dumps(message)
+    # Not JSON so just send it as is
+    except ValueError:
+      data = message
+
+    response = self._session.post(url, data=data, headers=headers)
+    
+    if response.status_code != requests.codes.ok:
+      raise requests.exceptions.HTTPError(response.text)
+
+    return response.json()
