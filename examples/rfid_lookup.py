cfbd6b5f26e80cd7a50a1b5182848d2a63e552b8
Brent Strysko <brentstrysko@brentstrysko.com>
2014-10-27 16:14:33 -0400

    RFID lookup and users projects examples.  Close #3.

---
+++ examples/rfid_lookup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from rc import APIClient
+import requests
+import sys
+
+if __name__ == '__main__':
+  # If the base variables RC_PUBLIC_KEY and RC_PRIVATE_KEY 
+  # are not set you need to pass the public and private key
+  # values into this constructor
+  client = APIClient()
+
+  rfid = input("RFID: ")
+
+  try:
+    user_id = client.rfid(rfid)
+  except requests.exceptions.HTTPError as e:
+    print("{}({})".format(e.detail, e.errno))
+    sys.exit(1)
+
+  user = client.user(user_id)
+
+  print("This user is {}".format(user['username']))
---
+++ examples/users_projects.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from rc import APIClient
+import requests
+import sys
+
+if __name__ == '__main__':
+  # If the base variables RC_PUBLIC_KEY and RC_PRIVATE_KEY 
+  # are not set you need to pass the public and private key
+  # values into this constructor
+  client = APIClient()
+
+  username = input("username: ")
+
+  users = client.users(username=username)
+  
+  if len(users) == 0:
+    print("Invalid username")
+    sys.exit(1)
+  else:
+    user = users[0]
+    
+    user_id = user['id']
+    projects = client.projects(leaders=user_id)
+
+    for project in projects:
+      print(project['name'])
